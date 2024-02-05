eba28bf2199c560e35d7487a6574ff0f81f85bb1
Pavel Umanskiy <114307635+PavelUmanskiy@users.noreply.github.com>
2024-02-05 15:53:30 +0300

    Update models, start working on views

--- survey_project/main/models.py
+++ survey_project/main/models.py
@@ -2,15 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Survey(models.Model):
+    author = models.ForeignKey(to=User, null=True, on_delete=models.SET_NULL)
+    last_update = models.DateField(auto_now=True)
+    description = models.TextField(max_length=2048)
+
+
 class Question(models.Model):
+    survey = models.ForeignKey(to=Survey, on_delete=models.CASCADE)
     next_question = models.ForeignKey(
         to='Question',
         null=True,
         blank=True,
-        on_delete=models.CASCADE
+        on_delete=models.SET_NULL
     )
     question_text = models.TextField(max_length=2048)
-    
+
 
 class Answer(models.Model):
     respondent = models.ForeignKey(
@@ -18,6 +25,7 @@ class Answer(models.Model):
         null=True,
         on_delete=models.SET_NULL
     )
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
     answer_text = models.TextField(max_length=2048)
-    answer_date = models.DateField(auto_now=True)
+    last_update = models.DateField(auto_now=True)
     
--- survey_project/main/views.py
+++ survey_project/main/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Question, Answer
+
+
+@login_required
+def index_view(request: HttpRequest) -> HttpResponse:
+    query = """
+    SELECT *
+    FROM public.main_question AS mq
+    JOIN
+    WHERE 
+    """
+    questions = Question.objects.raw()
+    return render(request, 'template_name.html', {'questions': questions})
