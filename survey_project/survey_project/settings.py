e2d6e4749f3c2db094d7451da4acac0cdb61deef
Pavel Umanskiy <114307635+PavelUmanskiy@users.noreply.github.com>
2024-02-05 15:17:45 +0300

    Small settings tweaks

--- survey_project/survey_project/settings.py
+++ survey_project/survey_project/settings.py
@@ -13,9 +13,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+else:
+    raise FileNotFoundError('.env not found')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
